571ade56a7614e707effaedca1d305586135da82
andrenivisnk <andrenivinsk@gmail.com>
2023-02-18 16:07:37 +0300

    lesson7 start

--- app/src/main/java/com/vk/lifecycle_aware_components_in_android/monitor/UnavailableConnectionLifecycleOwner.kt
+++ app/src/main/java/com/vk/lifecycle_aware_components_in_android/monitor/UnavailableConnectionLifecycleOwner.kt
@@ -1,10 +1,25 @@
 package com.vk.lifecycle_aware_components_in_android.monitor
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class UnavailableConnectionLifecycleOwner @Inject constructor() {
+class UnavailableConnectionLifecycleOwner @Inject constructor() : LifecycleOwner {
 
+    val lifecycleRegistry : LifecycleRegistry = LifecycleRegistry(this)
 
+    override fun getLifecycle(): Lifecycle {
+        return lifecycleRegistry
+    }
+
+    fun onConnectionAvailable(){
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+    }
+
+    fun onConnectionLost(){
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+    }
 }
